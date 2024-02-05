c14f153b4fc6380f1dd29ec7a3328a9cdf53f5be
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-02-05 22:35:13 +0900

    feat:주문방법

--- app/cart/order/page.tsx
+++ app/cart/order/page.tsx
@@ -6,27 +6,26 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <>
-      <div className="mx-[10%] bg-black text-white mt-12 text-center py-4">
-        <div className="w-full px-4 space-y-2">
+      <div className="max-w-[1280px] mx-automt-12 text-center p-4">
+        <div className="w-full p-4 bg-black text-white ">
           <p className="text-2xl sm:text-3xl p-6 border-b mb-4">주문이 완료되었습니다.</p>
           <p className="break-keep max-w-80 mx-auto">
             하단의 카톡아이디로 주문자 성함과 전화번호를 알려주시면 빠르게 결제 안내 도와드리겠습니다.
           </p>
-          <Link href={'www.kakao.com'} className="flex justify-center items-center gap-4">
+          <Link href={'www.kakao.com'} className="flex justify-center items-center gap-4 mt-4">
             <div className="mt-2 p-1 bg-[#FFEB3B] rounded-lg w-12 h-12 relative">
               <Image src={'/kakao.svg'} width={0} height={0} sizes="100" style={{width: '100%'}} alt={`kakao login`} />
             </div>
-            <p className="text-xl text-[#FFEB3B]">카톡 링크</p>
+            <p className="text-xl text-[#FFEB3B]">카카오톡 대화하기</p>
           </Link>
-        </div>
-        <div className="flex gap-4 justify-evenly mx-auto md:w-1/2 mt-10">
-          <Link href={`/shop`} className="border px-4 p-2">
-            <p>계속둘러보기</p>
-          </Link>
-
-          <Link href={`/myorder`} className="border px-4 p-2">
-            <p>주문내역확인하기</p>
-          </Link>
+          <div className="flex gap-4 justify-evenly mx-auto md:w-1/2 mt-14">
+            <Link href={`/shop`} className="border px-4 p-2">
+              <p>계속둘러보기</p>
+            </Link>
+            <Link href={`/myorder`} className="border px-4 p-2">
+              <p>주문내역확인하기</p>
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
 
+import Image from 'next/image';
+
 function Page() {
-  return <div>주문 방법</div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center mt-20 gap-20">
+      <h3 className="text-3xl">주문 방법</h3>
+      <div className="w-full bg-slate-200 p-16 flex flex-col min-[900px]:flex-row items-center justify-center gap-8">
+        <div className="p-4 w-full min-[900px]:w-1/3">
+          <div className="w-full relative border-2 border-black">
+            <Image
+              src={'/step1.png'}
+              alt="step1"
+              sizes="100"
+              width={0}
+              height={0}
+              style={{objectFit: 'cover', width: '100%'}}
+            />
+          </div>
+          <article className="mt-4 p-2 min-h-28">
+            <h5 className="text-2xl font-semibold">1. 주문하기</h5>
+            <p>원하는 상품을 카트에 담고, 쇼핑카트 아이콘을 클릭하여 담긴 상품을 확인 후 주문합니다.</p>
+          </article>
+        </div>
+        <div className="p-4 w-1/3">
+          <div className="w-full relative border-2 border-black">
+            <Image
+              src={'/step2.png'}
+              alt="step1"
+              sizes="100"
+              width={0}
+              height={0}
+              style={{objectFit: 'cover', width: '100%'}}
+            />
+          </div>
+          <article className="mt-4 p-2 min-h-28">
+            <h5 className="text-2xl font-semibold">2. 카카오톡 대화하기</h5>
+            <p>주문 후 주문완료 페이지에 카카오톡 대화하기를 클릭합니다.</p>
+          </article>
+        </div>
+        <div className="p-4 w-1/3">
+          <div className="w-full relative border-2 border-black">
+            <Image
+              src={'/step1.png'}
+              alt="step1"
+              sizes="100"
+              width={0}
+              height={0}
+              style={{objectFit: 'cover', width: '100%'}}
+            />
+          </div>
+          <article className="mt-4 p-2 min-h-28">
+            <h5 className="text-2xl font-semibold">3. 결제 및 배송</h5>
+            <p>이후 성함, 연락처만 카톡으로 알려주시면 확인하여 안내해드리겠습니다</p>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
