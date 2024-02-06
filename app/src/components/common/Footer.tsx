b2fd868f104ec9cd893c4c863c6071ebb3f43222
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-02-06 14:47:04 +0900

    design : 최종 수정

--- app/src/components/common/Footer.tsx
+++ app/src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <footer className="w-full p-8 bg-navy flex flex-col justify-center items-start h-40 text-sub6">
+    <footer className="w-full p-8 bg-black flex flex-col justify-center items-start h-40 text-sub6">
       <p>SWORDSUPPLY</p>
       <p className="text-sm text-sub6 break-keep">
         Copyright 2024. <strong>SWORDSUPPLY</strong> All rights reserved.
