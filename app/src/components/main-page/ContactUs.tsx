d48d2c4ff1d89066afa1a5a0269d07eee1a74f3d
rimsunwoo <dlatjsdn205@naver.com>
2024-02-06 14:26:52 +0900

    contact us

--- app/src/components/main-page/ContactUs.tsx
+++ app/src/components/main-page/ContactUs.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import Image from 'next/image';
-
 function ContactUs() {
   return (
-    <div className="w-full bg-slate-100 p-14">
-      <div className="max-w-[1080px] mx-auto flex text-en justify-between">
-        <Image src={'/ContactUsImg.jpg'} alt="contact us" width={0} height={0} sizes="100%" style={{width: '60%'}} />
-        <h3 className="text-end text-4xl font-semibold mb-6 font-roboto">Contact Us</h3>
+    <div className="w-full bg-[url('/bannerBg.png')] bg-slate-100 p-14">
+      <div className="max-w-[1080px] mx-auto flex items-center">
+        <h3 className="text-white text-end text-4xl font-semibold mr-10 font-roboto">Contact Us</h3>
+        <a
+          target="_blank"
+          href="https://prfl.link/@sword"
+          className="border p-2 rounded-md bg-black text-white hover:opacity-75">
+          문의하기 →
+        </a>
       </div>
     </div>
   );
