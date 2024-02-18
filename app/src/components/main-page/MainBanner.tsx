173f3fa8b011129359a4da0bff908c862bf6b84e
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-02-18 22:51:27 +0900

    이미지 파일명 수정

--- app/src/components/main-page/MainBanner.tsx
+++ app/src/components/main-page/MainBanner.tsx
@@ -10,10 +10,10 @@ function MainBanner() {
       <div className="w-full mx-auto ">
         <Carousel autoplay autoplaySpeed={6000}>
           <div className="flex justify-center items-center w-full p-5 pb-7 bg-[rgba(0,0,0,0.5)] shadow-[1px_4px_7px_0_rgba(0,0,0,0.02)]">
-            <div className="bg-[url('/bannerImgs/메인이벤트배너1.jpg')] h-[35vh] md:h-[50vh] bg-center bg-contain bg-no-repeat w-full" />
+            <div className="bg-[url('/bannerImgs/메인이벤트1.jpg')] h-[35vh] md:h-[50vh] bg-center bg-contain bg-no-repeat w-full" />
           </div>
           <div className="flex justify-center items-center w-full p-5 pb-7 bg-[rgba(0,0,0,0.5)] shadow-[1px_4px_7px_0_rgba(0,0,0,0.02)]">
-            <div className="bg-[url('/bannerImgs/메인이벤트배너2.jpg')] h-[35vh] md:h-[50vh] bg-center bg-contain bg-no-repeat" />
+            <div className="bg-[url('/bannerImgs/메인이벤트2.jpg')] h-[35vh] md:h-[50vh] bg-center bg-contain bg-no-repeat" />
           </div>
           <div className="flex flex-col items-start ">
             <div className="flex flex-col gap-8 text-white text-nowrap p-5 pb-7 bg-[rgba(0,0,0,0.5)] shadow-[1px_4px_7px_0_rgba(0,0,0,0.02)]">
